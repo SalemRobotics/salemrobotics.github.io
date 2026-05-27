4b01a30958c23d7c89d17c708990980c37e3e942
Cooper Bouchard <cobouchard11@gmail.com>
2026-05-26 22:34:02 -0400

    Attempt to fix lighthouse metrics & add formatting
    - Tried fixing perf/loading issues by using the Astro Fonts API
    - Added prettier (so huge diff :/)
    - Added robots.txt

--- astro.config.ts
+++ astro.config.ts
@@ -1,34 +1,56 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
 import { cpus } from "node:os";
 
 export default defineConfig({
-    prefetch: true,
-    site: "https://frc6324.com",
-    devToolbar: {
-        enabled: false,
+  build: {
+    concurrency: cpus().length,
+    format: "file",
+    inlineStylesheets: "always",
+  },
+  devToolbar: {
+    enabled: false,
+  },
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Devil Bauer",
+      cssVariable: "--font-devil-bauer",
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/devil_bauer.woff2"],
+            weight: "normal",
+            style: "normal",
+          },
+        ],
+      },
     },
+    {
+      provider: fontProviders.fontsource(),
+      name: "Oswald",
+      cssVariable: "--font-oswald",
+    },
+  ],
+  integrations: [sitemap()],
+  prefetch: true,
+  server: {
+    host: true,
+    port: 6324,
+  },
+  site: "https://frc6324.com",
+  vite: {
     build: {
-        concurrency: cpus().length,
-        format: "file"
+      minify: "terser",
+      cssMinify: "lightningcss",
     },
-    server: {
-        host: true,
-        port: 6324
+    resolve: {
+      alias: {
+        "@": "/src",
+        "@assets": "/src/assets",
+      },
     },
-    vite: {
-        build: {
-            minify: "terser",
-            cssMinify: "lightningcss"
-        },
-        resolve: {
-            alias: {
-                "@": "/src",
-                "@assets": "/src/assets"
-            }
-        }
-    },
-    integrations: [sitemap()]
+  },
 });
